43e64cf8a1932a53b330344b031afb4d066ddb26
Francesco Cina <ufoscout@gmail.com>
2019-06-24 12:56:39 +0200

    move auth to core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,12 +2,15 @@ pub mod config;
 pub mod error;
 pub mod module;
 pub mod service;
+pub mod web;
 
 use crate::error::LightSpeedError;
 use log::info;
+use crate::service::auth::InMemoryRolesProvider;
 
 #[derive(Clone)]
 pub struct CoreModule {
+    pub auth: service::auth::AuthService<InMemoryRolesProvider>,
     pub jwt: service::jwt::JwtService,
 }
 
@@ -17,8 +20,10 @@ impl CoreModule {
         info!("Creating CoreModule with configuration:\n{:#?}", config);
 
         let jwt = service::jwt::JwtService::new(&config.jwt);
-
-        CoreModule { jwt }
+        let auth = service::auth::AuthService::new(InMemoryRolesProvider::new(
+            vec![]
+        ));
+        CoreModule { jwt, auth }
     }
 }
 
@@ -27,4 +32,39 @@ impl module::Module for CoreModule {
         info!("Core start");
         Ok(())
     }
+}
+
+#[cfg(test)]
+pub mod test_root {
+
+    use lazy_static::lazy_static;
+    use ls_logger::config::LoggerConfig;
+    use ls_logger::setup_logger;
+    use std::sync::Mutex;
+
+    lazy_static! {
+        static ref INITIALIZED: Mutex<bool> = Mutex::new(false);
+    }
+
+    pub fn init_context() {
+        let mut init = INITIALIZED.lock().unwrap();
+        if !*init {
+            println!("Initialize context");
+            start_logger();
+            *init = true;
+        }
+    }
+
+    fn start_logger() {
+        println!("Init logger");
+
+        let conf = LoggerConfig {
+            level: String::from("trace"),
+            stdout_output: true,
+            stderr_output: false,
+            file_output_path: None,
+        };
+        setup_logger(&conf).unwrap();
+    }
+
 }
