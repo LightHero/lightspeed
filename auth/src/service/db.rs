edd75c790dbee12b863d70b91475c7e29b7a9035
Francesco Cina <ufoscout@gmail.com>
2019-07-21 12:12:02 +0200

    bump to c3p0 0.8

--- auth/src/service/db.rs
+++ auth/src/service/db.rs
@@ -1,20 +1,18 @@
-use c3p0::prelude::*;
+use c3p0::*;
 use include_dir::*;
 use ls_core::error::LightSpeedError;
-use c3p0_migrate::{C3p0MigrateBuilder, C3P0_MIGRATE_TABLE_DEFAULT};
 use std::convert::TryInto;
-use c3p0_migrate::migration::Migrations;
 
 const MIGRATIONS: Dir = include_dir!("./src_resources/db/migrations");
 
 #[derive(Clone)]
 pub struct AuthDbService {
-    c3p0: C3p0
+    c3p0: C3p0Builder
 }
 
 impl AuthDbService {
 
-    pub fn new(c3p0: C3p0) -> Self {
+    pub fn new(c3p0: C3p0Builder) -> Self {
         AuthDbService {
             c3p0
         }
@@ -22,19 +20,19 @@ impl AuthDbService {
 
     pub fn start(&self) -> Result<(), LightSpeedError> {
 
-        let migrate_table_name = format!("AUTH_{}", C3P0_MIGRATE_TABLE_DEFAULT);
-        let migrations: Migrations = (&MIGRATIONS).try_into().map_err(|err|
+        let migrate_table_name = format!("AUTH_{}", migrate::C3P0_MIGRATE_TABLE_DEFAULT);
+        let migrations: migrate::Migrations = (&MIGRATIONS).try_into().map_err(|err|
             LightSpeedError::ModuleStartError {
                 message: format!("AuthDbService failed to start: {}", err)
             }
         )?;
 
-        let migrate = C3p0MigrateBuilder::new()
+        let migrate = self.c3p0.migrate()
             .with_table_name(migrate_table_name)
             .with_migrations(migrations)
             .build();
 
-        migrate.migrate(&self.c3p0).map_err(|err|
+        migrate.migrate().map_err(|err|
             LightSpeedError::ModuleStartError {
                 message: format!("AuthDbService failed to start: {}", err)
             }
