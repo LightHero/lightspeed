39f72fd0a7521ecb39fb747d3b18e5f9c38b1e22
Francesco Cina <ufoscout@gmail.com>
2019-08-04 08:58:00 +0200

    Add token unit tests

--- auth/src/service/token.rs
+++ auth/src/service/token.rs
@@ -3,7 +3,7 @@ use crate::repository::token::{TokenRepository};
 use c3p0::*;
 use ls_core::config::UIConfig;
 use ls_core::error::LightSpeedError;
-use ls_core::utils::current_epoch_seconds;
+use ls_core::utils::*;
 use crate::config::AuthConfig;
 
 #[derive(Clone)]
@@ -23,7 +23,7 @@ impl TokenService {
         let issued_at = current_epoch_seconds();
         let expire_at_epoch = issued_at + (self.auth_config.token_activation_validity_minutes * 60);
         let token = NewModel::new(TokenData{
-            token: "".to_owned(),
+            token: new_hyphenated_uuid(),
             token_type,
             username,
             expire_at_epoch
@@ -32,15 +32,9 @@ impl TokenService {
     }
 
     pub fn generate_public_token_url(&self, token: &TokenModel) -> String {
-        match &token.data.token_type {
-            TokenType::AccountActivation => {
-                format!("{}{}{}", self.ui_config.public_domain, self.auth_config.activation_token_ui_url, token.data.token)
-            }
-            TokenType::ResetPassword => {
-                format!("{}{}{}", self.ui_config.public_domain, self.auth_config.reset_password_token_ui_url, token.data.token)
-            }
-        }
+        generate_public_token_url(&self.auth_config, &self.ui_config, token)
     }
+
 
     pub fn fetch_by_token(&self, conn: &PgConnection, token: &str, validate: bool) -> Result<Option<TokenModel>, LightSpeedError> {
         let token_model = self.token_repo.fetch_by_token(conn, token)?;
@@ -55,6 +49,95 @@ impl TokenService {
 
     pub fn delete(&self, conn: &PgConnection, token_model: TokenModel) -> Result<u64, LightSpeedError> {
         Ok(self.token_repo.delete(conn, &token_model)?)
+    }
+
+}
+
+fn generate_public_token_url(auth_config: &AuthConfig, ui_config: &UIConfig, token: &TokenModel) -> String {
+    match &token.data.token_type {
+        TokenType::AccountActivation => {
+            format!("{}{}{}", ui_config.public_domain, auth_config.activation_token_ui_url, token.data.token)
+        }
+        TokenType::ResetPassword => {
+            format!("{}{}{}", ui_config.public_domain, auth_config.reset_password_token_ui_url, token.data.token)
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+
+    use super::*;
+
+    #[test]
+    fn should_generate_activation_url() {
+        // Arrange
+        let domain = new_hyphenated_uuid();
+        let token_ui_url = new_hyphenated_uuid();
+
+        let auth_config = AuthConfig {
+            reset_password_token_ui_url: "".to_owned(),
+            activation_token_ui_url: token_ui_url.clone(),
+            auth_email_account_created_recipient: "".to_owned(),
+            token_activation_validity_minutes: 1
+        };
+
+        let ui_config = UIConfig {
+            public_domain: domain.clone()
+        };
+
+        let token = Model{
+            id: 0,
+            version: 0,
+            data: TokenData {
+                token: new_hyphenated_uuid(),
+                token_type: TokenType::AccountActivation,
+                username: "ufoscout".to_owned(),
+                expire_at_epoch: 1
+            }
+        };
+
+        // Act
+        let url = generate_public_token_url(&auth_config, &ui_config, &token);
+
+        // Assert
+        assert_eq!(format!("{}{}{}", domain, token_ui_url, token.data.token), url);
+    }
+
+    #[test]
+    fn should_generate_reset_password_url() {
+        // Arrange
+        let domain = new_hyphenated_uuid();
+        let token_ui_url = new_hyphenated_uuid();
+
+        let auth_config = AuthConfig {
+            reset_password_token_ui_url: token_ui_url.clone(),
+            activation_token_ui_url: "".to_owned(),
+            auth_email_account_created_recipient: "".to_owned(),
+            token_activation_validity_minutes: 1
+        };
+
+        let ui_config = UIConfig {
+            public_domain: domain.clone()
+        };
+
+        let token = Model{
+            id: 0,
+            version: 0,
+            data: TokenData {
+                token: new_hyphenated_uuid(),
+                token_type: TokenType::ResetPassword,
+                username: "ufoscout".to_owned(),
+                expire_at_epoch: 1
+            }
+        };
+
+        // Act
+        let url = generate_public_token_url(&auth_config, &ui_config, &token);
+
+        // Assert
+        assert_eq!(format!("{}{}{}", domain, token_ui_url, token.data.token), url);
+
     }
 
 }
--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,8 +1,14 @@
 use chrono::prelude::Local;
+use uuid::Uuid;
 
 /// Returns the number of non-leap seconds since January 1, 1970 0:00:00 UTC
 /// (aka "UNIX timestamp").
 #[inline]
 pub fn current_epoch_seconds() -> i64 {
     Local::now().timestamp()
+}
+
+#[inline]
+pub fn new_hyphenated_uuid() -> String {
+    Uuid::new_v4().to_hyphenated().to_string()
 }
