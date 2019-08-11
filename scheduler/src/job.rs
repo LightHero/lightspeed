b30813e1eec01b190e3b81cfb891942824fc137e
Francesco Cina <ufoscout@gmail.com>
2019-08-11 15:14:36 +0200

    Separate scheduler from job struct

--- scheduler/src/job.rs
+++ scheduler/src/job.rs
@@ -4,53 +4,39 @@ use chrono::{DateTime, Utc};
 use std::convert::TryInto;
 use std::sync::{Mutex, RwLock};
 
-pub struct Job {
-    function: Mutex<Box<Fn() -> Result<(), Box<std::error::Error>> + Send>>,
-    group: String,
-    name: String,
+pub struct JobScheduler {
+    pub job: Job,
     schedule: Schedule,
     next_run_at: Mutex<Option<DateTime<Utc>>>,
     last_run_at: Mutex<Option<DateTime<Utc>>>,
-    is_active: bool,
-    is_running: RwLock<bool>,
 }
 
-impl Job {
+impl JobScheduler {
     pub fn new<
-        G: Into<String>,
-        N: Into<String>,
-        S: TryInto<Schedule>,
-        F: Fn() -> Result<(), Box<std::error::Error>> + Send,
+        S: TryInto<Schedule>
     >(
-        group: G,
-        name: N,
         schedule: S,
-        function: F,
+        job: Job,
     ) -> Result<Self, SchedulerError>
-    where
-        F: 'static,
-        SchedulerError: std::convert::From<<S as std::convert::TryInto<Schedule>>::Error>,
+        where
+            SchedulerError: std::convert::From<<S as std::convert::TryInto<Schedule>>::Error>,
     {
         // Determine the next time it should run
         let schedule = schedule.try_into()?;
         let next_run_at = schedule.next(&None);
 
-        Ok(Job {
-            function: Mutex::new(Box::new(function)),
-            name: name.into(),
-            group: group.into(),
+        Ok(JobScheduler {
+            job,
             schedule,
             next_run_at: Mutex::new(next_run_at),
             last_run_at: Mutex::new(None),
-            is_running: RwLock::new(false),
-            is_active: true,
         })
     }
 
     /// Returns true if this job is pending execution.
     pub fn is_pending(&self) -> bool {
         // Check if paused
-        if !self.is_active {
+        if !self.job.is_active {
             return false;
         }
 
@@ -59,6 +45,55 @@ impl Job {
             &Utc::now() >= next_run_at
         } else {
             false
+        }
+    }
+
+    /// Run the job immediately and re-schedule it.
+    pub fn run(&self) -> Result<(), Box<std::error::Error>> {
+        // Execute the job function
+        let run_result = self.job.run();
+
+        let now = Some(Utc::now());
+
+        // Determine the next time it should run
+        let mut next_run_at = self.next_run_at.lock().unwrap();
+        *next_run_at = self.schedule.next(&now);
+
+        // Save the last time this ran
+        let mut last_run_at = self.last_run_at.lock().unwrap();
+        *last_run_at = now;
+
+        run_result
+    }
+}
+
+pub struct Job {
+    function: Mutex<Box<Fn() -> Result<(), Box<std::error::Error>> + Send>>,
+    group: String,
+    name: String,
+    is_active: bool,
+    is_running: RwLock<bool>,
+}
+
+impl Job {
+    pub fn new<
+        G: Into<String>,
+        N: Into<String>,
+        F: Fn() -> Result<(), Box<std::error::Error>> + Send,
+    >(
+        group: G,
+        name: N,
+        function: F,
+    ) -> Self
+    where
+        F: 'static
+    {
+        Job {
+            function: Mutex::new(Box::new(function)),
+            name: name.into(),
+            group: group.into(),
+            is_running: RwLock::new(false),
+            is_active: true,
         }
     }
 
@@ -101,16 +136,6 @@ impl Job {
                 })?;
             (function)()
         };
-
-        let now = Some(Utc::now());
-
-        // Determine the next time it should run
-        let mut next_run_at = self.next_run_at.lock().unwrap();
-        *next_run_at = self.schedule.next(&now);
-
-        // Save the last time this ran
-        let mut last_run_at = self.last_run_at.lock().unwrap();
-        *last_run_at = now;
 
         self.set_running(false)?;
 
@@ -157,23 +182,24 @@ pub mod test {
         let (tx, rx) = channel();
         let tx_clone = tx.clone();
 
-        let job = Arc::new(
-            Job::new("g", "n", Duration::new(1, 0), move || {
+        let job_scheduler = Arc::new(
+                JobScheduler::new(Duration::new(1, 0),
+            Job::new("g", "n", move || {
                 println!("job - started");
                 tx_clone.send("").unwrap();
                 println!("job - Trying to get the lock");
                 let _lock = lock_clone.lock().unwrap();
                 println!("job - lock acquired");
                 Ok(())
-            })
+            }))
             .unwrap(),
         );
 
-        assert!(!job.is_running().unwrap());
+        assert!(!job_scheduler.job.is_running().unwrap());
 
         {
             let _lock = lock.lock().unwrap();
-            let job_clone = job.clone();
+            let job_clone = job_scheduler.clone();
             std::thread::spawn(move || {
                 println!("starting job");
                 job_clone.run().unwrap();
@@ -181,10 +207,10 @@ pub mod test {
                 tx.send("").unwrap();
             });
             rx.recv().unwrap();
-            assert!(job.is_running().unwrap());
+            assert!(job_scheduler.job.is_running().unwrap());
         }
 
         rx.recv().unwrap();
-        assert!(!job.is_running().unwrap());
+        assert!(!job_scheduler.job.is_running().unwrap());
     }
 }
--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2,31 +2,34 @@ pub mod error;
 pub mod job;
 pub mod schedule;
 
-use crate::job::Job;
+use crate::job::{Job, JobScheduler};
 use log::*;
-use std::sync::Arc;
+use std::sync::{Arc};
+use std::convert::TryInto;
+use crate::schedule::Schedule;
+use crate::error::SchedulerError;
 
 #[derive(Default)]
-pub struct Scheduler {
-    jobs: Vec<Arc<Job>>,
+pub struct JobExecutor {
+    jobs: Vec<Arc<JobScheduler>>,
 }
 
-impl Scheduler {
+impl JobExecutor {
     pub fn new() -> Self {
-        Scheduler::default()
+        JobExecutor::default()
     }
 
-    /// Returns true if the Scheduler contains no jobs.
+    /// Returns true if the JobExecutor contains no jobs.
     pub fn is_empty(&self) -> bool {
         self.jobs.is_empty()
     }
 
-    /// Returns the number of jobs in the Scheduler.
+    /// Returns the number of jobs in the JobExecutor.
     pub fn len(&self) -> usize {
         self.jobs.len()
     }
 
-    /// Clear the Scheduler, removing all jobs.
+    /// Clear the JobExecutor, removing all jobs.
     pub fn clear(&mut self) {
         self.jobs.clear()
     }
@@ -41,48 +44,48 @@ impl Scheduler {
         false
     }
 
-    /// Run pending jobs in the Scheduler.
+    /// Run pending jobs in the JobExecutor.
     pub fn run_pending(&mut self) {
-        for job in &self.jobs {
+        for job_scheduler in &self.jobs {
             //println!("check JOB IS PENDING: {}", job.is_pending());
-            if job.is_pending() {
-                match job.is_running() {
+            if job_scheduler.is_pending() {
+                match job_scheduler.job.is_running() {
                     Ok(is_running) => {
                         //println!("JOB IS RUNNING? {}", is_running);
                         if !is_running {
-                            let job_clone = job.clone();
+                            let job_clone = job_scheduler.clone();
                             std::thread::spawn(move || {
                                 info!(
                                     "Start execution of Job [{}/{}]",
-                                    job_clone.group(),
-                                    job_clone.name()
+                                    job_clone.job.group(),
+                                    job_clone.job.name()
                                 );
                                 match job_clone.run() {
                                     Ok(()) => {
                                         info!(
                                             "Execution of Job [{}/{}] completed successfully",
-                                            job_clone.group(),
-                                            job_clone.name()
+                                            job_clone.job.group(),
+                                            job_clone.job.name()
                                         );
                                     }
                                     Err(err) => {
                                         error!(
                                             "Execution of Job [{}/{}] completed with errors. Err: {}",
-                                            job_clone.group(),
-                                            job_clone.name(),
+                                            job_clone.job.group(),
+                                            job_clone.job.name(),
                                             err
                                         );
                                     }
                                 }
                             });
                         } else {
-                            debug!("Job [{}/{}] is pending but already running. It will not be executed.", job.group(), job.name())
+                            debug!("Job [{}/{}] is pending but already running. It will not be executed.", job_scheduler.job.group(), job_scheduler.job.name())
                         }
                     }
                     Err(err) => error!(
                         "Cannot start execution of Job [{}/{}] because status is unknown. Err: {}",
-                        job.group(),
-                        job.name(),
+                        job_scheduler.job.group(),
+                        job_scheduler.job.name(),
                         err
                     ),
                 }
@@ -90,9 +93,12 @@ impl Scheduler {
         }
     }
 
-    /// Adds a job to the Scheduler.
-    pub fn add_job(&mut self, job: Job) {
-        self.jobs.push(Arc::new(job));
+    /// Adds a job to the JobExecutor.
+    pub fn add_job<S: TryInto<Schedule>>(&mut self, schedule: S, job: Job) -> Result<(), SchedulerError>
+    where
+    SchedulerError: std::convert::From<<S as std::convert::TryInto<Schedule>>::Error> {
+        self.jobs.push(Arc::new(JobScheduler::new(schedule, job)?));
+        Ok(())
     }
 }
 
@@ -107,27 +113,27 @@ pub mod test {
 
     #[test]
     fn should_not_run_an_already_running_job() {
-        let mut scheduler = Scheduler::new();
+        let mut executor = JobExecutor::new();
 
         let count = Arc::new(AtomicUsize::new(0));
         let count_clone = count.clone();
 
         let (tx, rx) = channel();
 
-        scheduler.add_job(
-            Job::new("g", "n", Duration::new(0, 1), move || {
+        executor.add_job(
+            Duration::new(0, 1),
+            Job::new("g", "n", move || {
                 tx.send("").unwrap();
                 println!("job - started");
                 count_clone.fetch_add(1, Ordering::SeqCst);
                 std::thread::sleep(Duration::new(1, 0));
                 Ok(())
             })
-            .unwrap(),
-        );
+        ).unwrap();
 
         for i in 0..100 {
             println!("run_pending {}", i);
-            scheduler.run_pending();
+            executor.run_pending();
             std::thread::sleep(Duration::new(0, 2));
         }
 
@@ -138,57 +144,57 @@ pub mod test {
     }
 
     #[test]
-    fn a_running_job_should_not_block_the_scheduler() {
-        let mut scheduler = Scheduler::new();
+    fn a_running_job_should_not_block_the_executor() {
+        let mut executor = JobExecutor::new();
 
         let (tx, rx) = channel();
 
         let count_1 = Arc::new(AtomicUsize::new(0));
         let count_clone_1 = count_1.clone();
         let tx_1 = tx.clone();
-        scheduler.add_job(
-            Job::new("g", "n", Duration::new(0, 1), move || {
+        executor.add_job(
+            Duration::new(0, 1),
+            Job::new("g", "n", move || {
                 tx_1.send("").unwrap();
                 println!("job 1 - started");
                 count_clone_1.fetch_add(1, Ordering::SeqCst);
                 std::thread::sleep(Duration::new(1, 0));
                 Ok(())
             })
-            .unwrap(),
-        );
+        ).unwrap();
 
         let count_2 = Arc::new(AtomicUsize::new(0));
         let count_2_clone = count_2.clone();
         let tx_2 = tx.clone();
-        scheduler.add_job(
-            Job::new("g", "n", Duration::new(0, 1), move || {
+        executor.add_job(
+            Duration::new(0, 1),
+            Job::new("g", "n", move || {
                 tx_2.send("").unwrap();
                 println!("job 2 - started");
                 count_2_clone.fetch_add(1, Ordering::SeqCst);
                 std::thread::sleep(Duration::new(1, 0));
                 Ok(())
             })
-            .unwrap(),
-        );
+        ).unwrap();
 
         let count_3 = Arc::new(AtomicUsize::new(0));
         let count_3_clone = count_3.clone();
         let tx_3 = tx.clone();
-        scheduler.add_job(
-            Job::new("g", "n", Duration::new(0, 1), move || {
+        executor.add_job(
+            Duration::new(0, 1),
+            Job::new("g", "n", move || {
                 tx_3.send("").unwrap();
                 println!("job 3 - started");
                 count_3_clone.fetch_add(1, Ordering::SeqCst);
                 std::thread::sleep(Duration::new(1, 0));
                 Ok(())
             })
-            .unwrap(),
-        );
+        ).unwrap();
 
         let before_millis = Utc::now().timestamp_millis();
         for i in 0..100 {
             println!("run_pending {}", i);
-            scheduler.run_pending();
+            executor.run_pending();
             std::thread::sleep(Duration::new(0, 1_000_000));
         }
         let after_millis = Utc::now().timestamp_millis();
@@ -201,5 +207,27 @@ pub mod test {
         assert_eq!(count_1.load(Ordering::SeqCst), 1);
         assert_eq!(count_2.load(Ordering::SeqCst), 1);
         assert_eq!(count_3.load(Ordering::SeqCst), 1);
+    }
+
+    //#[test]
+    fn timezone() {
+        let mut executor = JobExecutor::new();
+
+        let schedule = "0 19 13 * * *";
+
+        executor.add_job(
+            schedule,
+            Job::new("g", "n", move || {
+                println!("job 1 - started");
+                std::thread::sleep(Duration::new(1, 0));
+                Ok(())
+            })
+        ).unwrap();
+
+        println!("start loop");
+        loop {
+            executor.run_pending();
+            std::thread::sleep(Duration::new(1,0))
+        }
     }
 }
