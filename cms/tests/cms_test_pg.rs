3909f85c6bfc14cbed990d3a592558523ed2c10a
ufoscout <ufoscout@gmail.com>
2020-03-21 23:19:27 +0100

    Fix IT for latest PG docker image

--- cms/tests/cms_test_pg.rs
+++ cms/tests/cms_test_pg.rs
@@ -17,15 +17,21 @@ lazy_static! {
     static ref DOCKER: clients::Cli = clients::Cli::default();
     pub static ref SINGLETON: MaybeSingle<(
         CmsModule<RepoManager>,
-        Container<'static, clients::Cli, images::postgres::Postgres>
+        Container<'static, clients::Cli, images::generic::GenericImage>
     )> = MaybeSingle::new(|| init());
 }
 
 fn init() -> (
     CmsModule<RepoManager>,
-    Container<'static, clients::Cli, images::postgres::Postgres>,
+    Container<'static, clients::Cli, images::generic::GenericImage>,
 ) {
-    let node = DOCKER.run(images::postgres::Postgres::default());
+    let node = DOCKER.run(images::generic::GenericImage::new("postgres:11-alpine")
+        .with_wait_for(images::generic::WaitFor::message_on_stderr(
+            "database system is ready to accept connections",
+        ))
+        .with_env_var("POSTGRES_DB", "postgres")
+        .with_env_var("POSTGRES_USER", "postgres")
+        .with_env_var("POSTGRES_PASSWORD", "postgres"));
 
     let manager = PostgresConnectionManager::new(
         format!(
