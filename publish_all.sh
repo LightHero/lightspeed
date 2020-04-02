609fc59ae844a320e81ef4b3234d0ea892678241
ufoscout <ufoscout@gmail.com>
2020-04-02 12:31:09 +0200

    bump version

---
+++ publish_all.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+declare -a publish_list=(
+    "logger"
+    "scheduler"
+    "cache"
+    "core"
+    "auth"
+    "cms"
+    "email"
+)
+
+for i in "${publish_list[@]}"
+do
+    LINE_SEPARATOR='--------------------------------------------------------'
+
+    cd $i
+    echo $LINE_SEPARATOR
+    echo 'C3p0 - Run Cargo publish for [' $i ']'
+    echo $LINE_SEPARATOR
+
+    cargo publish
+    sleep 20
+    cd ..
+    rc=$?
+    if [[ $rc -ne 0 ]] ; then
+        echo "Failure publishing $i"; exit $rc
+    fi
+
+done
