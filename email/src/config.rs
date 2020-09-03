01969f34b35e35d47fa2f5a3db258fd346ac3fb4
ufoscout <ufoscout@gmail.com>
2020-09-03 13:31:54 +0200

    Add fixed_receiver config option

--- email/src/config.rs
+++ email/src/config.rs
@@ -22,11 +22,30 @@ pub struct EmailClientConfig {
 
     #[structopt(long, env = "LS_EMAIL_SERVER_USE_TLS", default_value = "false")]
     pub server_use_tls: Boolean,
+
+    #[structopt(
+    long,
+    env = "LS_EMAIL_FORWARD_ALL_EMAILS_TO_FIXED_RECIPIENTS",
+    value_delimiter = ";"
+    )]
+    pub forward_all_emails_to_fixed_recipients: Option<Vec<String>>,
 }
 
 impl EmailClientConfig {
     pub fn build() -> Self {
         let app = Self::clap().setting(structopt::clap::AppSettings::AllowExternalSubcommands);
         Self::from_clap(&app.get_matches())
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn should_build_config() {
+        let config = EmailClientConfig::build();
+        assert!(config.forward_all_emails_to_fixed_recipients.is_none());
     }
 }
--- email/src/repository/email.rs
+++ email/src/repository/email.rs
@@ -6,6 +6,8 @@ use crate::repository::no_ops_email::NoOpsEmailClient;
 use lightspeed_core::error::LightSpeedError;
 use std::str::FromStr;
 use std::sync::Arc;
+use crate::repository::fixed_recipient_email::FixedRecipientEmailClient;
+use log::*;
 
 #[async_trait::async_trait]
 pub trait EmailClient: Send + Sync {
@@ -19,11 +21,26 @@ pub trait EmailClient: Send + Sync {
 }
 
 pub fn new(email_config: EmailClientConfig) -> Result<Arc<dyn EmailClient>, LightSpeedError> {
-    match &email_config.client_type {
-        EmailClientType::Full => Ok(Arc::new(FullEmailClient::new(email_config)?)),
-        EmailClientType::InMemory => Ok(Arc::new(InMemoryEmailClient::new())),
-        EmailClientType::NoOps => Ok(Arc::new(NoOpsEmailClient::new())),
+
+    let client: Arc<dyn EmailClient> = match &email_config.client_type {
+        EmailClientType::Full => Arc::new(FullEmailClient::new(email_config.clone())?),
+        EmailClientType::InMemory => Arc::new(InMemoryEmailClient::new()),
+        EmailClientType::NoOps => Arc::new(NoOpsEmailClient::new()),
+    };
+
+    if let Some(recipients) = email_config.forward_all_emails_to_fixed_recipients {
+        warn!("All emails will be sent to the fixed recipients: {}", recipients.join("; "));
+        if recipients.is_empty() {
+            Err(LightSpeedError::ConfigurationError {
+                message: "Cannot build the email client. Based on the current config all emails should be sent to fixed recipients, but the recipient list is empty".to_owned()
+            })
+        } else {
+            Ok(Arc::new(FixedRecipientEmailClient::new(recipients, client)))
+        }
+    } else {
+        Ok(client)
     }
+
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
