103afbdcbe864c2a1198ffa0b0df141586bbd8b3
ufoscout <ufoscout@gmail.com>
2020-09-11 09:05:39 +0200

    Add language to LoginDto

--- auth/src/dto/login_dto.rs
+++ auth/src/dto/login_dto.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use typescript_definitions::TypeScriptify;
+use lightspeed_core::model::language::Language;
 
 #[derive(Serialize, Deserialize, TypeScriptify)]
 #[serde(rename_all = "camelCase")]
 pub struct LoginDto {
     pub username: String,
     pub password: String,
+    pub language: Language,
 }
