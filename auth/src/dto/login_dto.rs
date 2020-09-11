10ab4288c56c956e785f32c1268f0a51e98291da
ufoscout <ufoscout@gmail.com>
2020-09-11 14:54:22 +0200

    bump to 0.18.2

--- auth/src/dto/login_dto.rs
+++ auth/src/dto/login_dto.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
 use typescript_definitions::TypeScriptify;
-use lightspeed_core::model::language::Language;
 
 #[derive(Serialize, Deserialize, TypeScriptify)]
 #[serde(rename_all = "camelCase")]
