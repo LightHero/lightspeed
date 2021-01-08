bd13f30d97bfb3f135549b02ecff84ac28f47688
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-01-08 22:55:16 +0100

    Remve futures from deps where not needed

--- scheduler/src/job.rs
+++ scheduler/src/job.rs
@@ -2,10 +2,11 @@ use crate::error::SchedulerError;
 use crate::scheduler::Scheduler;
 use chrono::{DateTime, Utc};
 use chrono_tz::Tz;
-use futures::future::FutureExt;
 use log::*;
+use futures_util::FutureExt;
 use std::panic::AssertUnwindSafe;
-use tokio::macros::support::{Future, Pin};
+use std::future::Future;
+use std::pin::Pin;
 use tokio::sync::{Mutex, RwLock};
 
 pub struct JobScheduler {
