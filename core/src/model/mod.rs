203ae2faee799bea767f969b713b0c50694a8420
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-02-12 16:57:29 +0100

    Add data to ModelWithOwner

--- core/src/model/mod.rs
+++ core/src/model/mod.rs
@@ -8,19 +8,20 @@ pub mod language;
 pub mod model_dto;
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize, TypeScriptify)]
-pub struct ModelWithOwner {
+pub struct ModelWithOwner<D> {
     pub id: i64,
     pub version: i32,
     pub user_id: i64,
+    pub data: D,
 }
 
-impl Owned for ModelWithOwner {
+impl <D> Owned for ModelWithOwner<D> {
     fn get_owner_id(&self) -> i64 {
         self.user_id
     }
 }
 
-impl WithIdAndVersion for ModelWithOwner {
+impl <D> WithIdAndVersion for ModelWithOwner<D> {
     fn get_id(&self) -> i64 {
         self.id
     }
@@ -30,12 +31,24 @@ impl WithIdAndVersion for ModelWithOwner {
     }
 }
 
-impl <T: Owned + WithIdAndVersion> From<&T> for ModelWithOwner {
+impl <T: Owned + WithIdAndVersion, D> From<(&T, D)> for ModelWithOwner<D> {
+    fn from((model, data): (&T, D)) -> Self {
+        ModelWithOwner {
+            id: model.get_id(),
+            version: model.get_version(),
+            user_id: model.get_owner_id(),
+            data,
+        }
+    }
+}
+
+impl <T: Owned + WithIdAndVersion> From<&T> for ModelWithOwner<()> {
     fn from(model: &T) -> Self {
         ModelWithOwner {
             id: model.get_id(),
             version: model.get_version(),
-            user_id: model.get_owner_id()
+            user_id: model.get_owner_id(),
+            data: (),
         }
     }
 }
