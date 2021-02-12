818dfc74a9983dabeb2cab8c6f46a3af8070447b
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-02-12 10:01:53 +0100

    Add ModelWithOwner

--- core/src/model/mod.rs
+++ core/src/model/mod.rs
@@ -1,3 +1,28 @@
+use crate::service::auth::Owned;
+use crate::service::validator::ownership::WithIdAndVersion;
+
 pub mod boolean;
 pub mod language;
 pub mod model_dto;
+
+pub struct ModelWithOwner {
+    pub id: i64,
+    pub version: i32,
+    pub user_id: i64,
+}
+
+impl Owned for ModelWithOwner {
+    fn get_owner_id(&self) -> i64 {
+        self.user_id
+    }
+}
+
+impl WithIdAndVersion for ModelWithOwner {
+    fn get_id(&self) -> i64 {
+        self.id
+    }
+
+    fn get_version(&self) -> i32 {
+        self.version
+    }
+}
