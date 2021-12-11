7038e951f9a00b45dbfac9c43f30198dbea5ba1e
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-12-11 13:32:02 +0100

    Axum BinaryContent download - part 4

--- file_store/src/web/axum.rs
+++ file_store/src/web/axum.rs
@@ -1,19 +1,14 @@
 
-use axum_ext::{
-    body::StreamBody,
-    http::{header, StatusCode},
-    response::{Headers, IntoResponse},
-    routing::get,
-    Router,
-};
-use log::*;
-use std::net::SocketAddr;
-use tokio_util::io::ReaderStream;
 use crate::model::BinaryContent;
+use axum_ext::{
+    body::{boxed, BoxBody, Body, StreamBody},
+    http::{header, Response, response::Builder},
+    response::Headers,
+};
 use lightspeed_core::error::LightSpeedError;
-use axum_ext::body::{boxed, BoxBody, Body};
-use axum_ext::http::{Response, HeaderValue};
-use axum_ext::http::response::Builder;
+use log::*;
+use tokio_util::io::ReaderStream;
+use std::borrow::Cow;
 
 pub async fn into_response(
     content: BinaryContent<'_>,
@@ -21,7 +16,7 @@ pub async fn into_response(
     set_content_disposition: bool,
 ) -> Result<Response<BoxBody>, LightSpeedError> {
 
-    let (ct, body) = match content {
+    let (file_name, ct, body) = match content {
         BinaryContent::FromFs { file_path } => {
             debug!("Create HttpResponse from FS content");
             let ct = mime_guess::from_path(&file_path).first_or_octet_stream();
@@ -34,15 +29,30 @@ pub async fn into_response(
             let stream = ReaderStream::new(file);
             // convert the `Stream` into an `axum::body::HttpBody`
             let body = StreamBody::new(stream);
-            (ct, boxed(body))
+
+            let file_name = if let Some(file_name) = file_name {
+                Cow::Borrowed(file_name)
+            } else {
+                match file_path.file_name().to_owned() {
+                    Some(name) => Cow::Owned(name.to_string_lossy().as_ref().to_owned()),
+                    None => {
+                        return Err(LightSpeedError::BadRequest {
+                            message: "Provided path has no filename".to_owned(),
+                            code: "",
+                        })?;
+                    }
+                }
+            };
+
+            (file_name, ct, boxed(body))
         }
         BinaryContent::InMemory { content } => {
             debug!("Create HttpResponse from Memory content of {} bytes", content.len());
-            let file_name= file_name.unwrap_or("");
-            let ct = mime_guess::from_ext(&file_name).first_or_octet_stream();
-
+            let file_name = Cow::Borrowed(file_name.unwrap_or(""));
+            let path = std::path::Path::new(file_name.as_ref());
+            let ct = mime_guess::from_path(&path).first_or_octet_stream();
             let owned_vec: Vec<u8> = content.to_owned().into();
-            (ct, boxed(Body::from(owned_vec)))
+            (file_name, ct, boxed(Body::from(owned_vec)))
         }
     };
 
@@ -58,28 +68,30 @@ pub async fn into_response(
 
     let mut response_builder = Builder::new();
 
-    response_builder = response_builder.header(header::CONTENT_TYPE, "text/toml; charset=utf-8");
+    response_builder = response_builder.header(header::CONTENT_TYPE, format!("{}; charset=utf-8", ct));
 
-    // if set_content_disposition {
-    //     debug!("Set content disposition");
-    //     let disposition = match ct.type_() {
-    //         mime::IMAGE | mime::TEXT | mime::VIDEO => http::header::DispositionType::Inline,
-    //         _ => http::header::DispositionType::Attachment,
-    //     };
-    //     let mut parameters = vec![http::header::DispositionParam::Filename(String::from(filename.as_ref()))];
-    //     if !filename.is_ascii() {
-    //         parameters.push(http::header::DispositionParam::FilenameExt(http::header::ExtendedValue {
-    //             charset: http::header::Charset::Ext(String::from("UTF-8")),
-    //             language_tag: None,
-    //             value: filename.into_owned().into_bytes(),
-    //         }))
-    //     }
-    //     let cd = http::header::ContentDisposition { disposition, parameters };
-    //
-    //     res.append_header((http::header::CONTENT_DISPOSITION, cd));
-    // } else {
-    //     debug!("Ignore content disposition");
-    // };
+    if set_content_disposition {
+        debug!("Set content disposition");
+
+        let mut disposition = String::new();
+
+        match ct.type_() {
+            mime::IMAGE | mime::TEXT | mime::VIDEO => disposition.push_str("inline; "),
+            _ => disposition.push_str("attachment; "),
+        };
+
+        if !file_name.is_ascii() {
+
+        } else {
+            disposition.push_str("filename=\"");
+            disposition.push_str(file_name.as_ref());
+            disposition.push_str("\"");
+        }
+
+        response_builder = response_builder.header(header::CONTENT_DISPOSITION, disposition);
+    } else {
+        debug!("Ignore content disposition");
+    };
 
     response_builder.body(body)
         .map_err(|err| LightSpeedError::InternalServerError {
@@ -91,21 +103,19 @@ pub async fn into_response(
 #[cfg(test)]
 mod test {
     use super::*;
-    use axum_ext::http::{self, header, HeaderMap, Request};
     use axum_ext::{Router, AddExtensionLayer};
+    use axum_ext::extract::Extension;
+    use axum_ext::http::{self, Request, StatusCode};
     use axum_ext::routing::get;
+    use std::path::Path;
     use std::sync::Arc;
     use tower::ServiceExt; // for `app.oneshot()`
-
-    use std::path::Path;
-    use axum_ext::extract::Extension;
 
     async fn download(Extension(data): Extension<Arc<AppData>>) -> Result<Response<BoxBody>, LightSpeedError> {
         println!("Download called");
         into_response(data.content.clone(), data.file_name.clone(), data.set_content_disposition).await
     }
 
-    #[derive(Clone)]
     pub struct AppData {
         content: BinaryContent<'static>,
         file_name: Option<&'static str>,
@@ -126,7 +136,6 @@ mod test {
             .route("/download", get(download))
             .layer(AddExtensionLayer::new(data.clone()));
 
-
         // Act
         let resp = app
             .oneshot(Request::builder().method(http::Method::GET).uri("/download").body(Body::empty()).unwrap())
@@ -145,11 +154,11 @@ mod test {
     async fn should_download_bytes_with_content_disposition() {
         // Arrange
         let content = std::fs::read("./Cargo.toml").unwrap();
-        let data = AppData {
+        let data = Arc::new(AppData {
             content: BinaryContent::InMemory { content: content.clone().into() },
             file_name: Some("Cargo.toml"),
             set_content_disposition: true,
-        };
+        });
 
         let app = Router::new()
             .route("/download", get(download))
@@ -179,11 +188,11 @@ mod test {
         // Arrange
         let file_path = Path::new("./Cargo.toml").to_owned();
         let content = std::fs::read(&file_path).unwrap();
-        let data = AppData {
+        let data = Arc::new(AppData {
             content: BinaryContent::FromFs { file_path: file_path.clone() },
             file_name: Some("Cargo.toml"),
             set_content_disposition: false,
-        };
+        });
 
         let app = Router::new()
             .route("/download", get(download))
@@ -208,11 +217,11 @@ mod test {
         // Arrange
         let file_path = Path::new("./Cargo.toml").to_owned();
         let content = std::fs::read(&file_path).unwrap();
-        let data = AppData {
+        let data = Arc::new(AppData {
             content: BinaryContent::FromFs { file_path: file_path.clone() },
             file_name: None,
             set_content_disposition: true,
-        };
+        });
 
         let app = Router::new()
             .route("/download", get(download))
