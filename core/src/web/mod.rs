b6da2f3d17b8b8db1ca327707597d284ed2f3c9c
Francesco_Cina <ufoscout@gmail.com>
2024-01-30 13:19:22 +0100

    remove actix

--- core/src/web/mod.rs
+++ core/src/web/mod.rs
@@ -6,8 +6,6 @@ use http::{HeaderMap, HeaderValue, Request};
 use log::*;
 use std::sync::Arc;
 
-#[cfg(feature = "actix_web")]
-pub mod actix_web;
 #[cfg(feature = "axum")]
 pub mod axum;
 #[cfg(feature = "poem")]
--- file_store/src/web/mod.rs
+++ file_store/src/web/mod.rs
@@ -1,5 +1,2 @@
-#[cfg(feature = "actix_web")]
-pub mod actix_web;
-
 #[cfg(feature = "axum")]
 pub mod axum;
