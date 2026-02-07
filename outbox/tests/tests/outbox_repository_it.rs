b0dc05d8679bb4efd424f5a13a51fea6e28ea242
Francesco <ufoscout@gmail.com>
2026-02-07 15:41:40 +0100

    remove unused code

--- outbox/tests/tests/outbox_repository_it.rs
+++ outbox/tests/tests/outbox_repository_it.rs
@@ -12,24 +12,6 @@ use tokio::{task::JoinSet, time::sleep};
 use crate::data;
 
 #[test]
-fn should_delete_token() -> Result<(), LsError> {
-    tokio_test(async {
-        let data = data(false).await;
-        let outbox_module = &data.0;
-
-        let repo = outbox_module.repo_manager.outbox_repo();
-        let c3p0 = outbox_module.repo_manager.c3p0();
-
-        c3p0.transaction(async |conn| {
-            // Arrange
-
-            Ok(())
-        })
-        .await
-    })
-}
-
-#[test]
 fn test_repository() -> Result<(), LsError> {
     tokio_test(async {
         let data = data(false).await;
--- outbox/tests/tests/outbox_service_it.rs
+++ outbox/tests/tests/outbox_service_it.rs
@@ -1,35 +1,14 @@
-use std::time::Duration;
-
 use c3p0::C3p0Pool;
 use lightspeed_core::{error::LsError, service::random::LsRandomService};
 use lightspeed_outbox::{
     error::OutboxError,
-    model::{OutboxMessage, OutboxMessageData, OutboxMessageStatus},
+    model::{OutboxMessage, OutboxMessageStatus},
     repository::{OutboxRepository, OutboxRepositoryManager},
 };
 use lightspeed_test_utils::tokio_test;
 use serde_json::Value;
-use tokio::{task::JoinSet, time::sleep};
 
 use crate::{RepoManager, data};
-
-#[test]
-fn should_delete_token() -> Result<(), LsError> {
-    tokio_test(async {
-        let data = data(false).await;
-        let outbox_module = &data.0;
-
-        let repo = outbox_module.repo_manager.outbox_repo();
-        let c3p0 = outbox_module.repo_manager.c3p0();
-
-        c3p0.transaction(async |conn| {
-            // Arrange
-
-            Ok(())
-        })
-        .await
-    })
-}
 
 #[test]
 fn test_outbox_callback_success() -> Result<(), LsError> {
